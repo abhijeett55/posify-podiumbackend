506e432d553b32c89b1c12a725cd6e3278a179cc
abhijeett55 <100180572+abhijeett55@users.noreply.github.com>
2026-04-10 15:47:42 +0530

    fixes2

--- src/main/java/com/posify/podium/config/WebSecurityConfig.java
+++ src/main/java/com/posify/podium/config/WebSecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
